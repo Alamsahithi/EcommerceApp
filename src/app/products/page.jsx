6911094b8318d1d5b88d8cdd5e2f0ed3f0bf93a1
srinath-3898 <srinath3898@gmail.com>
2023-11-26 17:29:57 +0530

    Building Blocks of Ecommerce

---
+++ src/app/products/page.jsx
@@ -0,0 +1,35 @@
+"use client";
+import React, { useContext, useEffect } from "react";
+import styles from "./Products.module.css";
+import Card from "@/components/Card/Card";
+import ProductsContext from "@/store/Products/ProductsContext";
+import { AuthContext } from "@/store/Auth/AuthContext";
+import { useRouter } from "next/navigation";
+
+const Products = () => {
+  const router = useRouter();
+  const { token } = useContext(AuthContext);
+
+  const { products } = useContext(ProductsContext);
+
+  useEffect(() => {
+    if (!localStorage.getItem("token")) {
+      router.push("/login");
+    }
+  }, []);
+
+  return token ? (
+    <div className={styles.container}>
+      <p className={styles.music}>Music</p>
+      <div className={styles.products}>
+        {products.map((product) => (
+          <Card key={product?.id} product={product} />
+        ))}
+      </div>
+    </div>
+  ) : (
+    <></>
+  );
+};
+
+export default Products;
